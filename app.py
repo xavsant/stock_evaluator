915823fcba6bca64b4afd509b983fb44258fec4d
xavsant <xavierboons2021@economics.smu.edu.sg>
2024-12-02 18:48:34 +0100

    Changed page name to Black-Scholes-Merton

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if "tickers" not in st.session_state:
 pages = {
     "Functions": [
         st.Page("streamlit/monte_carlo_app.py", title="Monte Carlo Simulations"),
-        st.Page("streamlit/black_scholes_merton_app.py", title="Black Scholes Merton"),
+        st.Page("streamlit/black_scholes_merton_app.py", title="Black-Scholes-Merton"),
         st.Page("streamlit/sentiment_analysis_app.py", title="Sentiment Analysis"),
     ]
 }
